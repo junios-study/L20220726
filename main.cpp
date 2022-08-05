59a060f83481f6a2b6a27b7fe4397f1664eeeaee
junios <junios@naver.com>
2022-08-05 10:00:34 +0900

    SDL2 등록

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
-#include "Engine.h"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <iostream>
+#include "SDL.h"
+
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
 
 using namespace std;
 
-int main()
+int SDL_main(int argc, char* argv[])	
 {
-	Engine* MyEngine = GEngine;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* MyWindow = SDL_CreateWindow("Game", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED |
+		SDL_RENDERER_PRESENTVSYNC |
+		SDL_RENDERER_TARGETTEXTURE);
 
-	MyEngine->LoadMap("level1.txt");
-	MyEngine->Run();
+	//GPU 명령 리스트 넣기만 
+	SDL_SetRenderDrawColor(MyRenderer, 255, 0, 0, 255);
+	SDL_RenderClear(MyRenderer);
 
-	delete MyEngine;
+	SDL_RenderPresent(MyRenderer);
+	while (1);
+
+	SDL_DestroyRenderer(MyRenderer);
+	SDL_DestroyWindow(MyWindow);
+	SDL_Quit();
 
 	return 0;
 }
